a3d5a87a714d745c6fa326b76c43ae827aa8a8ee
Min Hu <lebonthe@gmail.com>
2024-08-16 22:12:05 +0800

    Initial Commit

---
+++ FirstProjectWithSwiftUI/FirstProjectWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectWithSwiftUIApp.swift
+//  FirstProjectWithSwiftUI
+//
+//  Created by Min Hu on 2024/8/16.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
